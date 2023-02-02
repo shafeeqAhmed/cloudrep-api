55b0e2e3ba6fd3506d9b22e2b28afd0d4a48aa0d
Talha Fayyaz <42764771+talhafayyaz123@users.noreply.github.com>
2023-02-02 18:28:01 +0500

    getCampaignCategory api

---
+++ app/Http/Resources/CampaignCustomResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CampaignCustomResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'uuid' => $this->resource->uuid,
+            'type' => $this->resource->type,
+            'cost_per_call' => $this->cost_per_call,
+            'cost_per_call_duration' => $this->cost_per_call_duration,
+        ];
+    }
+}
